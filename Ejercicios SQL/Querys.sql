a45f5740e6dbc9bfe0c410e6db7525c3d0f9c9fc
Support-Ficct <suport_ficct@hotmail.com>
2016-09-22 11:37:50 -0400

    Add files via upload

---
+++ Ejercicios SQL/Querys.sql
@@ -0,0 +1,40 @@
+-- Mostrar los productos de color ROJO
+
+-- Mostrar los Proveedores de las ciudades de Santa y La Paz`
+
+-- Mostrar los Almacenes de SC, CB, PA, BE Y TA
+
+-- Mostrar los productos existente en cada alamacen
+
+-- Mostrar los Almacenes de la ciudad de Cochabamba
+
+-- Mostrar los Productos Existentes en los almacenes de la ciudad de Santa Cruz
+
+-- Mostrar los Proveedores que Suministraron algun Producto 
+
+-- Mostrar los Productos Suminstrados por el Proveedor PRV3
+
+-- Mostrar los nombres de los proveedores que suministraron algun producto color rojo
+
+-- Mostrar los Productos existente en el Alamcenes 1,que fueron suministrado por el Proveedor PRV1 
+
+-- Mostrar los Productos de color Amarillo sunistrados por el Proveedor PRV2
+
+-- Mostrar los nombres de los productos sunistrados el año pasado por el proveedor PRV1
+
+-- Mostrar los nombres de los productos sunistrados el primer trimestre del año 2012 
+
+-- Mostrar las transaciones sumistradas cuyas cantidades superan al promedio de cantidades suministradas 
+
+-- Mostrar los nombres de proveedores y las ciudades donde los proveedores suministraron productos
+
+-- Mostrar los nombres de los proveedores que suministraron algun producto
+           
+-- Mostrar los nombres de los proveedores que suministraron algun producto color rojo
+
+-- Mostrar los nombres de los proveedores que suministraron algun producto fuera de su ciudad
+
+
+
+
+
